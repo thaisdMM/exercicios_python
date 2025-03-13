5689acb27051e95af8bed157545a0eeb34f5a9cf
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-13 18:15:43 +0100

    count 1 to number with range

---
+++ ex003.py
@@ -0,0 +1,4 @@
+# Contagem Progressiva: Peça um número ao usuário e exiba uma contagem de 1 até esse número.
+
+number = int(input("Digit a number: "))
+print(f"\nCount 1 to number {number} = {list(range(1,number +1))}")
