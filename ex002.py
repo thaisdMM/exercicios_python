02c6622ded55cab69cd1a960a135e2df09321100
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-13 18:01:07 +0100

    even or odd

---
+++ ex002.py
@@ -0,0 +1,7 @@
+# Par ou Ímpar: Peça um número e informe se ele é par ou ímpar.
+print("Check if it is EVEN or ODD!")
+number = int(input("Digit a number: "))
+if number % 2 == 0:
+   print(f"Number {number} is EVEN!!!")
+else:
+   print(f"Number {number} is ODD!!!")
