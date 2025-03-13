c9cd0282a9d5548117efe46e238fc9867ac33650
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-13 18:23:16 +0100

    for i in range

--- ex003.py
+++ ex003.py
@@ -1,4 +1,12 @@
 # Contagem Progressiva: Peça um número ao usuário e exiba uma contagem de 1 até esse número.
+# o exemplo abaixo cria uma lista com o range e gasta mais memória
+
+# number = int(input("Digit a number: "))
+# print(f"\nCount 1 to number {number} = {list(range(1,number +1))}")
+
+# Esse exemplo conta sem criar lista
 
 number = int(input("Digit a number: "))
-print(f"\nCount 1 to number {number} = {list(range(1,number +1))}")
+
+for i in range(1, number +1 ):
+   print(i)
