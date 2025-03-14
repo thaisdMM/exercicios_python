737b26459114a82f2cd9920397268b53386bfcb1
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-14 18:51:41 +0100

    program working

--- ex006.py
+++ ex006.py
@@ -1,21 +1,28 @@
 # 6️⃣ Lista de Compras: O usuário pode adicionar itens a uma lista de compras. Quando ele digitar “sair”, o programa exibe a lista completa.
 
-def shopping_item(shopping_list, item_list):
+def shopping_item(shopping_list, new_item_list):
+   item_list = ""
+   item_list = new_item_list
+   shopping_list.append(item_list)
+   print(f"The item: {item_list} has been added to the list successefully")
    return
 
 shopping_list = []
 
 while True:
    print("Shopping list")
-   print("1. Add items")
+   print("\n1. Add items")
    print("2. EXIT")
 
-   choice = input("Enter your choice: ").strip()   
+   choice = input("\nEnter your choice: ").strip()   
 
    if choice == "1":
-      shopping_item(shopping_list, item_list)
+      new_item_list = input("\nEnter the item for your shopping list: ")
+      
+      shopping_item(shopping_list, new_item_list)
 
    if choice == "2":
-      print("You have choosen to EXIT.")
+      print("\nYou have choosen to EXIT.")
       break
-print("Progam finished")
+print(f"\nThese are the items on your SHOPPING LIST: \n{shopping_list}") # não está adicionando nada
+print("\nProgam finished")
