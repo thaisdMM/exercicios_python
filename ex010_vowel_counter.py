b391cc9b9c41804afabfc7a4615f8676cb178ec1
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-04-30 18:41:41 +0200

    Update ex010_vowel_counter.py

--- ex010_vowel_counter.py
+++ ex010_vowel_counter.py
@@ -1,24 +1,35 @@
 # 1. Contador de vogais
 # Peça uma palavra e mostre quantas vogais ela tem.
 
+# word = input("Type a word: ").strip().lower()
+
+# print(
+#     f"""
+# In the word {word} we have:
+# {word.count('a')} a vowels
+# {word.count('e')} e vowels
+# {word.count('i')} i vowels
+# {word.count('o')} o vowels
+# {word.count('u')} u vowels
+# """
+# )
+# total = (
+#     word.count("a")
+#     + word.count("e")
+#     + word.count("i")
+#     + word.count("o")
+#     + word.count("u")
+# )
+# print(f"Total vowels = {total}")
+
+# with for
+
 word = input("Type a word: ").strip().lower()
+number_vowel = have_vowel = 0
+for letter in "aeiou":
+    have_vowel = word.count(letter)
+    number_vowel += have_vowel
 
-print(
-    f"""
-In the word {word} we have:
-{word.count('a')} a vowels
-{word.count('e')} e vowels
-{word.count('i')} i vowels
-{word.count('o')} o vowels
-{word.count('u')} u vowels
-"""
-)
-total = (
-    word.count("a")
-    + word.count("e")
-    + word.count("i")
-    + word.count("o")
-    + word.count("u")
-)
-print(f"Total vowels = {total}")
-
+    print(letter, end=" ")
+    print(f"Vowel: {have_vowel}")
+print(f"Total vowels = {number_vowel}")
