b11c35639b2f565a2c86a4ed4ba1ae20b36d34d0
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-01 14:29:04 +0200

    Update ex016_find_letter.py

--- ex016_find_letter.py
+++ ex016_find_letter.py
@@ -3,13 +3,8 @@
 
 phrase = input("Enter a phrase: ").strip().lower()
 find_letter = input("Choose a letter to search in the phrase: ").strip().lower()
-# print(find_letter)
-have_letter = first_appearence_phrase = 0
-for letter in phrase:
-    if letter == find_letter:
-        # print(letter, end=" ")
-        have_letter = phrase.count(letter)
-        first_appearence_phrase = phrase.find(find_letter)
+have_letter = phrase.count(find_letter)
+first_appearence_phrase = phrase.find(find_letter)
 print(f"We have the letter {find_letter} {have_letter} times in the phrase.")
 print(
     f"The first appearence of the letter {find_letter} in the phrase is: {first_appearence_phrase + 1}"
