43cad693a996fa9c49d2f68bef8355a70d6c7c73
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-17 11:48:45 +0200

    Update 001_mini_projeto_notas_alunos_media_disciplina.py
    - tentando validar matricula sem repetir

--- projetos/001_mini_projeto_notas_alunos_media_disciplina.py
+++ projetos/001_mini_projeto_notas_alunos_media_disciplina.py
@@ -12,27 +12,48 @@ lista_alunos = []
 linha = "=-" * 50
 while True:
     aluno["nome"] = input("Nome do aluno: ").strip().title()
-    # aluno["Matricula"] = int(input(f"Matricula do aluno {aluno['nome']} "))
-    if len(lista_alunos) == 0:
+    while True:
         matricula = int(input(f"Matrícula do aluno {aluno['nome']}: "))
-        aluno["matricula"] = matricula
-        print(aluno["matricula"])
-
-    if len(lista_alunos) != 0:
-        matricula = int(input(f"Matrícula do aluno {aluno['nome']}: "))
-        for valor in lista_alunos:
-            print(f"valor {valor}")
-            while True:
-                if matricula != valor["matricula"]:
-                    aluno["matricula"] = matricula
-                    print(
-                        f"Aluno {aluno['nome']} cadastrado com sucesso com a matricula {aluno['matricula']}"
-                    )
-                    break
+        for aluno_existente in lista_alunos:
+            if aluno_existente["matricula"] == matricula:
                 print(
-                    f"Matrícula já cadastrada em outro aluno. Por favor, digite uma matricula diferente."
+                    "Matrícula já cadastrada em outro aluno. Por favor, digite outra matrícula."
                 )
                 matricula = int(input(f"Matrícula do aluno {aluno['nome']}: "))
+        aluno["matricula"] = matricula
+        print(
+            f"Aluno {aluno['nome']} cadastrado com sucesso com a matricula {aluno['matricula']}"
+        )
+        break
+
+    # while True:
+    #     matricula = int(input(f"Matrícula do aluno {aluno['nome']}: "))
+    #     if any(aluno_existente["matricula"] == matricula: for aluno_existente in lista_alunos):
+    #             print("Matrícula já cadastrada em outro aluno. Por favor, digite outra matrícula.")
+    #     else:
+    #         #aluno["matricula"] = matricula
+    #         break
+    # aluno["Matricula"] = int(input(f"Matricula do aluno {aluno['nome']} "))
+    # if len(lista_alunos) == 0:
+    #     matricula = int(input(f"Matrícula do aluno {aluno['nome']}: "))
+    #     aluno["matricula"] = matricula
+    #     print(aluno["matricula"])
+
+    # if len(lista_alunos) != 0:
+    #     matricula = int(input(f"Matrícula do aluno {aluno['nome']}: "))
+    #     for valor in lista_alunos:
+    #         print(f"valor {valor}")
+    #         while True:
+    #             if matricula != valor["matricula"]:
+    #                 aluno["matricula"] = matricula
+    #                 print(
+    #                     f"Aluno {aluno['nome']} cadastrado com sucesso com a matricula {aluno['matricula']}"
+    #                 )
+    #                 break
+    #             print(
+    #                 f"Matrícula já cadastrada em outro aluno. Por favor, digite uma matricula diferente."
+    #             )
+    #             matricula = int(input(f"Matrícula do aluno {aluno['nome']}: "))
     lista_alunos.append(aluno.copy())
     print(f"Lista de alunos cadastrados: {lista_alunos}")
     while True:
