4c26e931ebc2b2d76fb8541c5da30a810d0e7ab9
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-17 16:00:56 +0200

    validação disciplina por nome e codigo

--- projetos/001_mini_projeto_notas_alunos_media_disciplina.py
+++ projetos/001_mini_projeto_notas_alunos_media_disciplina.py
@@ -7,30 +7,33 @@
 # 	•	Parte 4: Relatórios e médias.
 # 	•	Parte 5: Busca por aluno e refinamento final.
 
-aluno = {}
+
 lista_alunos = []
-disciplina = {}
 lista_disciplinas = [
-    {"nome": "MATEMÁTICA", "codigo": 101},
-    {"nome": "HISTÓRIA", "código": 102},
-    {"nome": "CIÊNCIAS", "codigo": 103}
+    {"nome": "Matematica", "codigo": 101},
+    {"nome": "Historia", "codigo": 102},
+    {"nome": "Ciencias", "codigo": 103},
 ]
-
-
 linha = "=-" * 50
 while True:
+    aluno = {}
     aluno["nome"] = input("Nome do aluno: ").strip().title()
     matricula = int(input(f"Matrícula do aluno {aluno['nome']}: "))
     while True:
-        if any(matricula_existente['matricula'] == matricula for matricula_existente in lista_alunos):
+        if any(
+            matricula_existente["matricula"] == matricula
+            for matricula_existente in lista_alunos
+        ):
             print(f"Matrícula já cadastrada. Por favor, digite outra matrícula.")
             matricula = int(input(f"Matrícula do aluno {aluno['nome']}: "))
         else:
             break
     aluno["matricula"] = matricula
-    print(f"{aluno['nome']} cadastrado com sucesso com a matrícula {aluno['matricula']}")
+    print(
+        f"{aluno['nome']} cadastrado com sucesso com a matrícula {aluno['matricula']}"
+    )
     lista_alunos.append(aluno.copy())
-    print(f"Lista de alunos cadastrados: {lista_alunos}")
+    # print(f"Lista de alunos cadastrados: {lista_alunos}")
     while True:
         continuar = (
             input("Quer continuar a cadastrar alunos? [S/N] ").strip().upper()[0]
@@ -48,32 +51,67 @@ for posicao, valor in enumerate(lista_alunos):
         print(f"{key} = {value:<40} ", end="")
     print()
 print(linha)
-print(f"As diciplinas já cadastradas são: ")
-#while True:
+print(f"As disciplinas já cadastradas são: ")
+print()
 for materia in lista_disciplinas:
     for key, value in materia.items():
         print(f"{key}: {value:<20} ", end="")
     print()
+print(linha)
 while True:
-    print(f"dicionario de disciplina: {disciplina}")
-    print(f"lista de disciplina: {lista_disciplinas}")
-    cadastro_disciplina = input("Quer cadastrar alguma disciplina nova? [S/N] ").strip().upper()[0]
+    disciplina = {}
+    # print(f"dicionario de disciplina: {disciplina}")
+    # print(f"lista de disciplina: {lista_disciplinas}")
+    cadastro_disciplina = (
+        input("Quer cadastrar alguma disciplina nova? [S/N] ").strip().title()[0]
+    )
     if cadastro_disciplina == "N":
         print("Escolheu não cadastrar novas disciplinas.")
         break
     if cadastro_disciplina == "S":
-        disciplina["nome"] = input("Qual o nome da nova disciplina que deseja cadastrar? ").strip().upper()
-        disciplina["codigo"] = int(input(f"qual o código da {disciplina['nome']} "))
-        print(f"dicionario de disciplina: {disciplina}")
+        nova_disciplina = (
+            input("Qual o nome da nova disciplina que deseja cadastrar? ")
+            .strip()
+            .title()
+        )
+        while True:
+            if any(
+                disciplina_existente["nome"] == nova_disciplina
+                for disciplina_existente in lista_disciplinas
+            ):
+                print(f"A disciplina {nova_disciplina} já está cadastrada.")
+                nova_disciplina = (
+                    input("Qual o nome da nova disciplina que deseja cadastrar? ")
+                    .strip()
+                    .title()
+                )
+            else:
+                break
+        disciplina["nome"] = nova_disciplina
+        codigo_nova_disciplina = int(input(f"Qual o código da {disciplina['nome']}? "))
+        while True:
+            if any(
+                codigo_existente["codigo"] == codigo_nova_disciplina
+                for codigo_existente in lista_disciplinas
+            ):
+                print(
+                    f"O código {codigo_nova_disciplina} já está associado a outra disciplina. Digite outro código!"
+                )
+                codigo_nova_disciplina = int(
+                    input(f"Qual o código da {disciplina['nome']}? ")
+                )
+            else:
+                break
+        disciplina["codigo"] = codigo_nova_disciplina
+        # print(f"dicionario de disciplina: {disciplina}")
         lista_disciplinas.append(disciplina.copy())
         disciplina.clear()
-        print(f"dicionario de disciplina: {disciplina}")
-
+        # print(f"dicionario de disciplina: {disciplina}")
     else:
         print("Resposta inválida. Digite S para sim ou N para não.")
-    #break
+    # break
 print(linha)
-print(f"As diciplinas já cadastradas são: ")
+print(f"As disciplinas já cadastradas são: ")
 for materia in lista_disciplinas:
     for key, value in materia.items():
         print(f"{key}: {value:<20} ", end="")
