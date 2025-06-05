0396c5081658912fcb043a78145d50debff3485a
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-06-05 15:12:48 +0200

    function to read integer add to menu

--- projetos/project_students_subjects_school.py/library/interface/project_interfaces.py
+++ projetos/project_students_subjects_school.py/library/interface/project_interfaces.py
@@ -1,4 +1,21 @@
-def linha(tamanho = 50):
+def leiaInt(msg):
+    while True:
+        try:
+            valor_digitado = input(msg)
+            valor_convertido = int(valor_digitado)
+        except (ValueError, TypeError):
+            print(
+                f"\033[0;31mErro! '{valor_digitado}' Por favor digite um número inteiro válido!\033[m"
+            )
+            continue
+        except KeyboardInterrupt:
+            print(f"\33[0;31mUsuário preferiu não digitar esse número.\033[m")
+            return 0
+        else:
+            return valor_convertido
+
+
+def linha(tamanho=50):
     return "-" * tamanho
 
 
@@ -17,3 +34,5 @@ def menu(lista):
         print(f"\033[33m{contador:2}\033[m - \033[34m{item}\033[m")
         contador += 1
     print(linha())
+    opcao = leiaInt("\033[32mBem vindo ao programa. Em que podemos ajudar? Digite a opção: \033[m")
+    return opcao
