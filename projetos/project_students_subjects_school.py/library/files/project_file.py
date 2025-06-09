f16fe9ce9a10ad81f8af58870b04646ea785ec96
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-06-09 19:42:52 +0200

    created new file functions

--- projetos/project_students_subjects_school.py/library/files/project_file.py
+++ projetos/project_students_subjects_school.py/library/files/project_file.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 
 def verificar_arquivo_existe(file_path):
@@ -7,6 +8,37 @@ def verificar_arquivo_existe(file_path):
         return file
     else:
         return None
+
+
+def criar_arquivo(file_path):
+    data = []
+    try:
+        with open(file_path, "x") as file:
+            pass
+    except FileExistsError:
+        print(f"O arquivo já existe")
+    except PermissionError:
+        print(f"Você não tem permissão para criar esse arquivo.")
+
+
+def subscrever_arquivo(file_paht, data):
+    try:
+        with open(file_paht, "w") as file:
+            json.dump(data, file, indent=4)
+            return file
+    except FileNotFoundError:
+        return "Arquivo não encontrado"
+    except PermissionError:
+        return "Você não tem permissão para criar esse arquivo."
+
+
+def append_arquivo(file_path, data):
+    try:
+        with open(file_path, "a") as append_file:
+            json.dump(data, append_file, indent=4)
+            return append_file
+    except PermissionError:
+        return "Você não tem permissão para criar esse arquivo."
 
 
 # def verificar_arquivo_existe(file_path):
